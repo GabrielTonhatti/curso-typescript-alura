56da406ff1aaa251b31216558bbc60ed40c3d2b6
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-12-02 22:42:55 -0300

    Inico da parte 3

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -3,44 +3,70 @@ import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
+
 export class NegociacaoController {
+
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+    private negociacoes: Negociacoes = new Negociacoes();
+    private negociacoesView: NegociacoesView = new NegociacoesView('#negociacoesView', true);
+    private mensagemView = new MensagemView('#mensagemView');
+
     constructor() {
-        this.negociacoes = new Negociacoes();
-        this.negociacoesView = new NegociacoesView('#negociacoesView', true);
-        this.mensagemView = new MensagemView('#mensagemView');
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
+        this.inputData = <HTMLInputElement>document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
+        this.inputValor = document.querySelector('#valor') as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
     }
-    adiciona() {
-        const negociacao = Negociacao.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
+
+    public adiciona(): void {
+        const t1 = performance.now();
+        // Zé, você já viu isso?
+        const negociacao: Negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
+
         if (!this.ehDiaUtil(negociacao.data)) {
             this.mensagemView.alert('Apenas negociações em dias úteis são aceitas');
+
             this.ocultarMensagem();
+
             return;
         }
+
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
+
         this.ocultarMensagem();
+
+        const t2 = performance.now();
+        console.log(`Tempo de execução do método adiciona: ${(t2 - t1) / 1000} segundos`);
     }
-    ehDiaUtil(data) {
+
+    private ehDiaUtil(data: Date) {
         return data.getDay() > DiaDaSenmana.DOMINGO && data.getDay() < DiaDaSenmana.SABADO;
     }
-    ocultarMensagem() {
+
+    private ocultarMensagem(): void {
         setTimeout(() => {
             this.mensagemView.limpaMensagem();
         }, 5000);
     }
-    limparFormulario() {
+
+    private limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
         this.inputData.focus();
     }
-    atualizaView() {
+
+    private atualizaView(): void {
         this.negociacoesView.update(this.negociacoes);
         this.mensagemView.update('Negociação adicionada com sucesso!');
     }
+
 }
